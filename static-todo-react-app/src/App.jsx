e9cc9f36c2a2fb01b96f54bdf2a63a3acb885e5c
Bruno Araujogit <bruno.correadearaujo@gmail.com>
2025-10-19 13:37:46 -0500

    Homework Static todo react app that contains cards

---
+++ static-todo-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+
+import ProfileCard from './Components/ProfileCard'
+import './App.css'
+import ProfileContainer from './Components/ProfileContainer'
+function App() {
+  
+  return (
+    <>
+      <div>
+          <ProfileCard/>
+          <ProfileContainer/>
+      </div>
+    </>
+  )
+}
+
+export default App
